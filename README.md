9464b464b96a498e9248c3ed9e2a4875282a9308
Enrique Lacal <enrique.lacal.bereslawski@ibm.com>
2020-12-17 16:53:18 +0000

    feat: Add ability to traverse tree and non-exact module name

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # GetDeps
+
+
+# Instructions
+
+Install:
+```
+go build && cp getdeps /usr/localbin
+```
+
+
+Use:
+```
+go mod graph > graph.txt
+getdeps graph.txt <module-name>
+```
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -12,8 +11,6 @@ import (
 
 type Graph map[string][]string
 
-// ReadLines reads a file, splits it into lines by the given splitter
-// and returns an array of strings
 func ReadLines(path, splitOn string) ([]string, error) {
 	content, readFileErr := ioutil.ReadFile(path)
 	if readFileErr != nil {
@@ -22,6 +19,81 @@ func ReadLines(path, splitOn string) ([]string, error) {
 	lines := strings.Split(string(content), splitOn)
 
 	return lines, nil
+}
+
+func tvc(graph Graph, find string) (results [][]string) {
+	visited := []string{}
+	for m, modules := range graph {
+		if len(modules) == 0 {
+			continue
+		}
+
+		for _, subTree := range tvcModules(graph, visited, modules, find) {
+			subTree = append(subTree, m)
+			results = append(results, subTree)
+		}
+
+	}
+
+	return results
+}
+
+func visitedAleady(visited []string, module string) bool {
+	for _, m := range visited {
+		if m == module {
+			return true
+		}
+	}
+	return false
+}
+
+func tvcModules(graph Graph, visited []string, modules []string, find string) (results [][]string) {
+	for _, module := range modules {
+		if visitedAleady(visited, module) {
+			continue
+		}
+		visited = append(visited, module)
+		if strings.Contains(module, find) {
+			return [][]string{[]string{module}}
+		}
+
+		if subModules, ok := graph[module]; ok {
+			for _, subTree := range tvcModules(graph, visited, subModules, find) {
+				subTree = append(subTree, module)
+				results = append(results, subTree)
+			}
+		}
+	}
+
+	return results
+}
+
+func getLongestPath(paths [][]string) []string {
+	longestPath := []string{}
+	for _, path := range paths {
+		if len(path) > len(longestPath) {
+			longestPath = path
+		}
+	}
+	return longestPath
+}
+
+func prettyPrint(path []string) {
+	for i, m := range path {
+		for v := 0; v < i; v++ {
+			fmt.Printf("  ")
+		}
+		fmt.Printf("|__ %s\n", m)
+	}
+}
+
+func revertPath(path []string) (rPath []string) {
+	rPath = []string{}
+	for i := len(path) - 1; i >= 0; i-- {
+		rPath = append(rPath, path[i])
+	}
+
+	return
 }
 
 func main() {
@@ -49,9 +121,9 @@ func main() {
 		graph[key] = append(graph[key], value)
 	}
 
-	b, err := json.MarshalIndent(graph[searchDep], "", "  ")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Print(string(b))
+	paths := tvc(graph, searchDep)
+	longestPath := getLongestPath(paths)
+	path := revertPath(longestPath)
+	prettyPrint(path)
+
 }
